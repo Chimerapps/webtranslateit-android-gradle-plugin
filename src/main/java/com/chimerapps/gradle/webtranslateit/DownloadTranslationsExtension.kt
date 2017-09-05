b7a3bc78e42433665c7329bb7a9376413580f8eb
Nicola Verbeeck <nicola@chimerapps.com>
2017-09-05 10:05:35 +0200

    Copyright

--- src/main/java/com/chimerapps/gradle/webtranslateit/DownloadTranslationsExtension.kt
+++ src/main/java/com/chimerapps/gradle/webtranslateit/DownloadTranslationsExtension.kt
@@ -1,3 +1,20 @@
+/*
+ * Copyright 2017 - Chimerapps BVBA
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
 package com.chimerapps.gradle.webtranslateit
 
 import com.chimerapps.gradle.webtranslateit.utils.exists
@@ -7,7 +24,6 @@ import org.gradle.api.Action
  * @author Nicola Verbeeck
  * @date 04/09/2017.
  */
-
 open class TranslationConfiguration(open var name: String = "default") {
 
     open var apiKey: String? = null
